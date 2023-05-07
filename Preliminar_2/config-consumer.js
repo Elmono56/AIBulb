1c2bde61486bfde71aeb331622906e39ac156782
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-05-07 13:11:09 -0600

    add consumer.js without test

---
+++ Preliminar_2/config-consumer.js
@@ -0,0 +1,47 @@
+const { Kafka } = require('kafkajs');
+const { MongoClient } = require('mongodb');
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9092']
+});
+
+const consumer = kafka.consumer({ groupId: 'content-update-group' });
+
+const mongoUri = 'mongodb://root:example@localhost:27017,localhost:27018,localhost:27019/mydatabase?replicaSet=rs0';
+const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function updateContent(id, contenido) {
+    try {
+        await client.connect();
+
+        const db = client.db();
+        const collection = db.collection('providers');
+
+        await collection.updateOne(
+            { id: id },
+            { $set: { content: contenido } },
+            { upsert: true }
+        );
+
+        await client.close();
+    } catch (err) {
+        console.error('Error updating content:', err);
+    }
+}
+
+async function run() {
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'content-update', fromBeginning: false });
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            const id = parseInt(message.key);
+            const contenido = JSON.parse(message.value);
+
+            await updateContent(id, contenido);
+        }
+    });
+}
+
+run().catch(console.error);
