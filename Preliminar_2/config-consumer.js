41b1bb4955943bd3bdfd297243e4300b7aaab754
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-05-13 02:25:30 -0600

    ready consumer

--- Preliminar_2/config-consumer.js
+++ Preliminar_2/config-consumer.js
@@ -7,37 +7,51 @@ const kafka = new Kafka({
 });
 
 const consumer = kafka.consumer({ groupId: 'content-update' });
-
+console.log('Testing consumer');
 const mongoUri = 'mongodb://localhost:27017/mydatabase';
 const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log('Testing mongo');
 
 async function updateContent(id, contenido) {
+    console.log('Updating content');
     try {
         await client.connect();
 
         const db = client.db();
         const collection = db.collection('usuarios');
 
-        await collection.updateOne({ id: id }, { $set: { contenido } });
+        const fieldsToUpdate = Object.keys(contenido); // Obtener los nombres de los campos a actualizar
+        const valuesToUpdate = Object.values(contenido); // Obtener los valores que se van a actualizar
+
+        const updateObj = {};
+        fieldsToUpdate.forEach((field, index) => {
+            updateObj[field] = valuesToUpdate[index]; // Combinar las llaves y valores en un objeto
+        });
+
+        await collection.updateOne({ id: id }, { $set: updateObj });
 
     } catch (err) {
         console.error('Error updating content:', err);
     } finally {
+        console.log('Closing mongo client');
         await client.close();
     }
 }
 
+
 async function run() {
     await consumer.connect();
     await consumer.subscribe({ topic: 'content-update', fromBeginning: true });
-
+    console.log('Testing consumer inside run()');
     await consumer.run({
-        eachMessage: async ({ topic, messages }) => {
-            console.log(messages);
-            const content = messages.value.toString();
-            const parsedContent = JSON.parse(content);
-            await updateContent(parsedContent.id, parsedContent.contenido);
-        }
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log('Testing consumer inside run() async');
+            // console.log(message);
+            const id = message.key.toString();
+            const contenido = JSON.parse(message.value.toString());
+            console.log('Testing consumer inside run() after parse');
+            await updateContent(id, contenido);
+        },
     });
 }
 
