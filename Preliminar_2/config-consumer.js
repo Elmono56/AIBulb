aeb06e7ace6b99d21d04d8772f12cdcf6a564dac
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-05-14 20:53:38 -0600

    fix consumer

--- Preliminar_2/config-consumer.js
+++ Preliminar_2/config-consumer.js
@@ -18,7 +18,7 @@ async function updateContent(id, contenido) {
         await client.connect();
 
         const db = client.db();
-        const collection = db.collection('usuarios');
+        const collection = db.collection('servicios');
 
         const fieldsToUpdate = Object.keys(contenido); // Obtener los nombres de los campos a actualizar
         const valuesToUpdate = Object.values(contenido); // Obtener los valores que se van a actualizar
@@ -28,7 +28,7 @@ async function updateContent(id, contenido) {
             updateObj[field] = valuesToUpdate[index]; // Combinar las llaves y valores en un objeto
         });
 
-        await collection.updateOne({ id: id }, { $set: updateObj });
+        await collection.updateOne({ ID: id }, { $set: updateObj });
 
     } catch (err) {
         console.error('Error updating content:', err);
